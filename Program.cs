112533b0a203be78a4c7ab6c16f5b0b000944ccd
Marshall181 <balazsersek729@gmail.com>
2025-11-13 18:37:34 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,51 +1,52 @@
-﻿while (true)
+﻿List<char> validCharacters = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ' };
+
+while (true)
 {
     Console.Write("Please write a message: ");
     string? message = Console.ReadLine();
     Console.Write("Please write a key (at least as long as the message): ");
     string? key = Console.ReadLine();
+    if (message is null || key is null)
+        continue;
+    else if (!ValidityCheck(message) || !ValidityCheck(key))
+        continue;
 
-    Encryption encryption = new Encryption(message, key);
-    string encoded = encryption.Encoder();
+    string encoded = Encoder(message, key);
     Console.WriteLine(encoded);
 }
 
-
-public class Encryption
+bool ValidityCheck(string text)
 {
-    public string Key { get; set; }
-    public string Message { get; set; }
-
-    private List<char> validCharacters = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ' };
-
-    public Encryption(string message, string key)
+    foreach (char c in text)
     {
-        Key = key;
-        Message = message;
+        if (!validCharacters.Contains(c))
+            return false;
     }
-
-    public string Encoder()
-    {
-        if (Message.Length > Key.Length && Message == null)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Key shorter than message or no message!");
-            Console.ForegroundColor= ConsoleColor.White;
-            return string.Empty;
-        }
-
-        List<char> encodedCharacters = new List<char>();
-
-        for (int i = 0; i < Message.Length; i++)
-        {
-            int charIndex = (validCharacters.IndexOf(Message[i]) + validCharacters.IndexOf(Key[i])) % 26;
-            encodedCharacters.Add(validCharacters[charIndex]);
-        }
-        string? encodedMessage = new string(encodedCharacters.ToArray());
-        return encodedMessage == null ? string.Empty : encodedMessage;
-    }
+    return true;
 }
+
+string Encoder(string message, string key)
+{
+    if (message.Length > key.Length || message == null)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Key is shorter than message or there is no message given!");
+        Console.ForegroundColor = ConsoleColor.White;
+        return string.Empty;
+    }
+
+    List<char> encodedCharacters = new List<char>();
+
+    for (int i = 0; i < message.Length; i++)
+    {
+        int charIndex = (validCharacters.IndexOf(message[i]) + validCharacters.IndexOf(key[i])) % 26;
+        encodedCharacters.Add(validCharacters[charIndex]);
+    }
+    string? encodedMessage = new string(encodedCharacters.ToArray());
+    return encodedMessage == null ? string.Empty : encodedMessage;
+}
+
 
 
 
